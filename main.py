4498be5834452170f998514c31b110b717c93f03
borks <steveminecraft899@gmail.com>
2024-02-06 03:33:37 -0600

    finished line formatting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import requests
 import time
+import shutil
+import math
 
 pygame.init()
 s_width = 1600
@@ -11,9 +13,31 @@ clock = pygame.time.Clock()
 big = pygame.font.SysFont("Manjari", 80)
 small = pygame.font.SysFont("Manjari", 30)
 blanks_text = ""
-type_text = ["hi", "doin", "ur", "mom"]
+type_text = [
+    "greeting",
+    "adjective to describe a person",
+    "noun",
+    "noun, plural",
+    "verb that two people can do, ending in -ing",
+    "verb that two people can do, ending in -ing",
+    "adjective to describe an activity",
+    "verb",
+    "name of some famous group",
+    "noun, plural",
+    "random fun fact",
+    "verb",
+    "body part",
+    "verb",
+    "project two people can work on, plural",
+    "sign off phrase",
+    "noun",
+    "adjective",
+]
 blanks_storage = []
 i = 0
+
+replaceable_text = "{}, {} Giancarlo Beritella. How’s the {}? How are the {}? I feel like it’s been ages since we {} at CMCB. I really enjoy {} with you, I feel like it is always very {}. We could {} about anything from {} to {} to how {}. Sometimes I just want to {} your {} and {} at what’s going on in there. We should work on more {} together. {}.\n- Christian, with some help from his ARTV 101 class\nP.S. I've never met anyone with the same {} as you. I think that's pretty {}."
+final_text = ""
 
 r = 0
 g = 0
@@ -23,6 +47,43 @@ b_timer = 2
 r_up = True
 g_up = True
 b_up = True
+
+
+def rgb():
+    global r, g, b, g_timer, b_timer, r_up, g_up, b_up
+    if r_up and r == 255:
+        r_up = False
+    if not r_up and r == 0:
+        r_up = True
+    if g_up and g == 255:
+        g_up = False
+    if not g_up and g == 0:
+        g_up = True
+    if b_up and b == 255:
+        b_up = False
+    if not b_up and b == 0:
+        b_up = True
+    if r_up:
+        r += 1
+    else:
+        r -= 1
+    if g_timer:
+        g_timer -= 1
+    else:
+        if g_up:
+            g += 1
+        else:
+            g -= 1
+        g_timer = 1
+    if b_timer:
+        b_timer -= 1
+    else:
+        if b_up:
+            b += 1
+        else:
+            b -= 1
+        b_timer = 2
+
 
 backspace = False
 backspace_start = None
@@ -59,41 +120,10 @@ while running:
         elif not backspace_rapid:
             backspace_rapid = time.time()
 
-    if r_up and r == 255:
-        r_up = False
-    if not r_up and r == 0:
-        r_up = True
-    if g_up and g == 255:
-        g_up = False
-    if not g_up and g == 0:
-        g_up = True
-    if b_up and b == 255:
-        b_up = False
-    if not b_up and b == 0:
-        b_up = True
-    if r_up:
-        r += 1
-    else:
-        r -= 1
-    if g_timer:
-        g_timer -= 1
-    else:
-        if g_up:
-            g += 1
-        else:
-            g -= 1
-        g_timer = 1
-    if b_timer:
-        b_timer -= 1
-    else:
-        if b_up:
-            b += 1
-        else:
-            b -= 1
-        b_timer = 2
+    rgb()
     screen.fill(pygame.Color(r, g, b))
 
-    if i < 4:
+    if i < len(type_text):
         blanks = pygame.font.Font.render(big, blanks_text, False, (255, 255, 255))
         blanks_r = blanks.get_rect()
         blanks_r.centerx = s_width // 2
@@ -111,44 +141,45 @@ while running:
     pygame.display.flip()
     clock.tick(60)
 
+final_text = replaceable_text.format(*blanks_storage)
+print(final_text)
+final_split = final_text.split("\n")
+to_split = final_split[0].split(" ")
+current_line = ""
+current_line_preview = ""
+lines = []
+for word in to_split:
+    current_line_preview += word + " "
+    if big.size(current_line_preview)[0] < math.ceil(s_width * 0.8):
+        current_line += word + " "
+    else:
+        lines.append(current_line)
+        current_line = word + " "
+        current_line_preview = word + " "
+lines.append(current_line)
+small_lines = [final_split[1]]
+current_line = ""
+current_line_preview = ""
+for word in final_split[2].split(" "):
+    current_line_preview += word + " "
+    if small.size(current_line_preview)[0] < math.ceil(s_width * 0.8):
+        current_line += word + " "
+    else:
+        small_lines.append(current_line)
+        current_line = word + " "
+        current_line_preview = word + " "
+small_lines.append(current_line)
+for line in lines:
+    print(line)
+for line in small_lines:
+    print(line)
 
 while generating:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             generating = False
 
-    if r_up and r == 255:
-        r_up = False
-    if not r_up and r == 0:
-        r_up = True
-    if g_up and g == 255:
-        g_up = False
-    if not g_up and g == 0:
-        g_up = True
-    if b_up and b == 255:
-        b_up = False
-    if not b_up and b == 0:
-        b_up = True
-    if r_up:
-        r += 1
-    else:
-        r -= 1
-    if g_timer:
-        g_timer -= 1
-    else:
-        if g_up:
-            g += 1
-        else:
-            g -= 1
-        g_timer = 1
-    if b_timer:
-        b_timer -= 1
-    else:
-        if b_up:
-            b += 1
-        else:
-            b -= 1
-        b_timer = 2
+    rgb()
     screen.fill(pygame.Color(r, g, b))
 
     pygame.display.flip()
